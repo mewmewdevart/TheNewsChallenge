868fbb8c606f971d73b2ade70ea69cf6a4aad350
mewUbuntu <mewmewdevart@gmail.com>
2025-02-16 16:13:26 -0300

    feat: settings to conect the databse in render and in backend

--- backend-the-news/config.py
+++ backend-the-news/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///newsletter_streaks.db')
+    DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///newsletter_streaks.db')
+    print(f"DATABASE_URL: {DATABASE_URL}") 
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    WEBHOOK_URL = os.getenv('WEBHOOK_URL', 'http://localhost:5000/webhook')
-    REGISTERED_EMAIL = os.getenv('REGISTERED_EMAIL', 'admin@waffle.com')
--- backend-the-news/run.py
+++ backend-the-news/run.py
@@ -1,19 +1,29 @@
-import os
+# run.py
 from flask import Flask, render_template
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from sqlalchemy import text
 from config import Config
 from app.routes import routes
-from app.models import db
+from app.database import db
 
 def create_app():
     app = Flask(__name__, template_folder="templates")
     app.config.from_object(Config)
-    
+
     db.init_app(app)
-    
+    Migrate(app, db)
+
+    # Database Connection Test
+    with app.app_context():
+        try:
+            db.session.execute(text('SELECT 1')) 
+            print("Database connection successful!")
+        except Exception as e:
+            print(f"Error connecting to the database: {e}")
+
     CORS(app, origins=["http://localhost:5173"])
-    
+
     app.register_blueprint(routes)
 
     @app.route('/')
@@ -23,11 +33,6 @@ def create_app():
     return app
 
 app = create_app()
-
-db_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'newsletter_streaks.db')
-if not os.path.exists(db_path):
-    with app.app_context():
-        db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
