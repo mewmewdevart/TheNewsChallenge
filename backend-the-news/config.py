962a499218ba8812ff197317f1a8c26dd7030775
mewUbuntu <mewmewdevart@gmail.com>
2025-02-16 20:30:01 -0300

    fix: adjustmenets in the variables

--- backend-the-news/config.py
+++ backend-the-news/config.py
@@ -1,7 +1,8 @@
 import os
 from dotenv import load_dotenv
 
-# load_dotenv() for dev 
+# Carrega as variáveis do .env (apenas para desenvolvimento)
+load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
