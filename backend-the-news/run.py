3c55a97c72ca26844e66d2ad7805a8e6e47d2d53
mewUbuntu <mewmewdevart@gmail.com>
2025-02-22 16:10:13 -0300

    fix: add the localhost in the cors run file

--- backend-the-news/run.py
+++ backend-the-news/run.py
@@ -25,7 +25,7 @@ def create_app():
         except Exception as e:
             print(f"Error connecting to the database: {e}")
 
-    CORS(app, origins=["https://the-news-letter-streaks.vercel.app", "https://thenewsletterstreakschallenge.onrender.com"])
+    CORS(app, origins=["http://localhost:5173/", "https://the-news-letter-streaks.vercel.app", "https://thenewsletterstreakschallenge.onrender.com"])
 
     app.register_blueprint(routes)
 
