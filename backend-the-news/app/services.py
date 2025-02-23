c8b8ee85be442194c3b85e7619ab16f5b78f34f4
mewUbuntu <mewmewdevart@gmail.com>
2025-02-23 16:12:43 -0300

    fix: adjustmenets in the update_streaks function with the new mentions

--- backend-the-news/app/services.py
+++ backend-the-news/app/services.py
@@ -1,5 +1,5 @@
 from app.models import NewsletterRead, db
-from .utils import calculate_streak
+from app.utils import calculate_streak
 
 def update_streaks():
     """Updates the newsletter read streaks for all users in the database."""
@@ -7,9 +7,11 @@ def update_streaks():
 
     for email in emails:
         email = email[0]
-        streak = calculate_streak(email)
+        streak_data = calculate_streak(email)
+        streak = streak_data['streak']
+        max_streak = streak_data['max_streak']
+        
+        NewsletterRead.query.filter_by(email=email).update({"streak": streak, "max_streak": max_streak})
+        db.session.commit()
 
-        NewsletterRead.query.filter_by(email=email).update({"streak": streak})
-
-    db.session.commit()
     print("Streaks updated successfully!")
